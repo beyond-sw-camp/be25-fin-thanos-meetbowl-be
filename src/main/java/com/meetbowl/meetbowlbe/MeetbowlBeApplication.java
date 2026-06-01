f1cffed14503929e4b8bfb7a538c7e3365750b8a
penep0 <pengco681@gmail.com>
2026-06-01 21:13:46 +0900

    chor: spring boot 초기 세팅

---
+++ src/main/java/com/meetbowl/meetbowlbe/MeetbowlBeApplication.java
@@ -0,0 +1,13 @@
+package com.meetbowl.meetbowlbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetbowlBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MeetbowlBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/meetbowl/meetbowlbe/MeetbowlBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meetbowl.meetbowlbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetbowlBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
